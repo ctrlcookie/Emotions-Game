ace3f37ccbeb4d6c233d58f24db87e43f51726df
ctrlcookie <regardingquiet@gmail.com>
2021-09-28 15:04:46 +0100

    Ryan's movement code

--- Emotional Game/Assets/Scripts/PlayerMovement.cs
+++ Emotional Game/Assets/Scripts/PlayerMovement.cs
@@ -6,93 +6,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float movementSpeed;
+    public Rigidbody2D rb;
 
-    [SerializeField] private Collider2D groundCheck;
-    [SerializeField] private LayerMask groundLayers;
-    public Rigidbody2D myRB;
-    public SpriteRenderer mySprite;
-    private float moveDir;
-   [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
-    bool canJump = true;
+    private float mx; //Horizontal Movement
+    private float my; //Vertical Movement
 
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        mySprite = GetComponentInChildren<SpriteRenderer>();
-        myRB = GetComponent<Rigidbody2D>();
+        mx = Input.GetAxisRaw("Horizontal");
+        my = Input.GetAxisRaw("Vertical");
 
+        if (mx != 0) { transform.localScale = new Vector3(mx, 1f, 1f); }
     }
 
     private void FixedUpdate()
     {
+        Vector2 movement = new Vector2(mx * movementSpeed, my * movementSpeed);
 
-        if (moveDir > 0)
-        {
-            mySprite.flipX = false;
-        }
-
-        if (moveDir < 0)
-        {
-            mySprite.flipX = true;
-        }
-
-        var moveAxis = Vector3.right * moveDir;
-
-        if (Mathf.Abs(myRB.velocity.x) < maxSpeed)
-        {
-            myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
-        }
-
-        if (groundCheck.IsTouchingLayers(groundLayers))
-        {
-            canJump = true;
-        }
-        else
-        {
-            canJump = false;
-        }
-
-        myRB.AddForce(transform.right * moveDir * moveSpeed);
-
-    }
-
-
-    public void Jump(InputAction.CallbackContext context)
-    {
-        Debug.Log("Up");
-        if (canJump)
-        {
-            if (context.started)
-            {
-                myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
-                canJump = false;
-            }
-        }
-
-    }
-
-    public void Left(InputAction.CallbackContext context) 
-    {
-        Debug.Log("Left");
-        moveDir = -1;
-
-        if (context.canceled)
-        {
-            moveDir = 0;
-        }
-    }
-
-    public void Right(InputAction.CallbackContext context)
-    {
-        Debug.Log("Right");
-        moveDir = 1;
-
-        if (context.canceled)
-        {
-            moveDir = 0;
-        }
+        rb.velocity = movement;
     }
 
 }
